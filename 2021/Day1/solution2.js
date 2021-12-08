f14ca1b03ca723e26ca9271969bc6a80d779b9eb
Matt Bunch <mattbunch@protonmail.com>
2021-12-08 23:26:04 +1300

    Day 1 Solution 2 optimized

--- 2021/Day1/solution2.js
+++ 2021/Day1/solution2.js
@@ -4,11 +4,9 @@ const countWindows = (inputArr) => {
   let counter = 0;
   let previousWindow;
   for (let i = 0; i < inputArr.length; i++) {
-    if (inputArr[i + 1] != null && inputArr[i + 2] != null) {
-      const window = inputArr[i] + inputArr[i + 1] + inputArr[i + 2];
-      if (window > previousWindow && previousWindow != null) counter++;
-      previousWindow = window;
-    }
+    const window = inputArr[i] + inputArr[i + 1] + inputArr[i + 2];
+    if (window > previousWindow && previousWindow != null) counter++;
+    previousWindow = window;
   }
   return counter;
 };
