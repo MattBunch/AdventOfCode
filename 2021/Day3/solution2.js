eef15e145e1c92bd0ae47aca4351332826006e4b
Matt Bunch <mattbunch@protonmail.com>
2021-12-10 18:17:34 +1300

    Day 3 Solution 2 optimized

--- 2021/Day3/solution2.js
+++ 2021/Day3/solution2.js
@@ -5,35 +5,27 @@ const lifeSupportRating = (inputArr) => {
     let ones = [];
     let zeroes = [];
     const wordLength = arrayCopy[0].length;
-
     for (let i = 0; i < wordLength; i++) {
       for (const str of arrayCopy) {
         str.charAt(i) === "0" ? zeroes.push(str) : ones.push(str);
       }
-
       const removeFromSet = (inputSet) => {
         arrayCopy = arrayCopy.filter((elem) => {
           return !inputSet.has(elem);
         });
       };
-
-      if (ones.length > zeroes.length || zeroes.length === ones.length) {
+      if (ones.length >= zeroes.length) {
         if (isOxygen) removeFromSet(new Set(zeroes));
         else removeFromSet(new Set(ones));
-      }
-
-      if (zeroes.length > ones.length) {
+      } else {
         if (isOxygen) removeFromSet(new Set(ones));
         else removeFromSet(new Set(zeroes));
       }
-
       ones = [];
       zeroes = [];
-
       if (arrayCopy.length == 1) return arrayCopy[0];
     }
   };
-
   return (
     parseInt(getBit([...inputArr], false), 2) *
     parseInt(getBit([...inputArr], true), 2)
