85b5a6bff164ca4b3f4defc63886103e9d853714
Matt Bunch <mattbunch@protonmail.com>
2021-12-10 18:16:44 +1300

    Day 3 Solution 1 optimized

--- 2021/Day3/solution1.js
+++ 2021/Day3/solution1.js
@@ -3,28 +3,25 @@ import { data } from "./input.js";
 const calculateOutput = (inputArr) => {
   let gamma = "";
   let epsilon = "";
-
-  const zeroes = [];
-  const ones = [];
+  let zeroes = 0;
+  let ones = 0;
   const wordLength = inputArr[0].length;
-
   for (let i = 0; i < wordLength; i++) {
     for (const str of inputArr) {
       const c = str.charAt(i);
-      if (c === "0") zeroes.push(c);
-      else ones.push(c);
+      if (c === "0") zeroes++;
+      else ones++;
     }
-    if (zeroes.length > ones.length) {
+    if (zeroes > ones) {
       gamma += "0";
       epsilon += "1";
     } else {
       gamma += "1";
       epsilon += "0";
     }
-    zeroes.length = 0;
-    ones.length = 0;
+    zeroes = 0;
+    ones = 0;
   }
-
   return parseInt(gamma, 2) * parseInt(epsilon, 2);
 };
 
