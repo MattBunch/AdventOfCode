042ef939da897a39eb20e63c044ded9b4132a404
Matt Bunch <mattbunch@protonmail.com>
2021-12-14 22:54:31 +1300

    Day 8 Solution 2 debug

--- 2021/Day8/solution2.js
+++ 2021/Day8/solution2.js
@@ -1,7 +1,8 @@
 import { data } from "./input.js";
 
 const findDigits = (input) => {
-  let output = "";
+  // let output = "";
+  const outputArr = [];
   for (const row of input) {
     const container = row.split("|").map((item) => item.trim());
     const uniqueSignalPatterns = container[0].split(" ");
@@ -9,79 +10,58 @@ const findDigits = (input) => {
     console.log(uniqueSignalPatterns);
     console.log(digitOutputValues);
 
-    const parseValues = (inputArr) => {
-      console.log(inputArr);
-      let zero, one, two, three, four, five, six, seven, eight, nine;
-      let segment = {};
-      const assignValues = () => {
-        for (const signal of inputArr) {
-          if (signal.length === 2) {
-            one = signal;
-            segment.c = signal.split()[0];
-            segment.f = signal.split()[1];
-          }
-          if (signal.length === 3) {
-            seven = signal;
-            segment.a = signal.split()[0];
-            segment.c = signal.split()[1];
-            segment.f = signal.split()[2];
-          }
-          if (signal.length === 4) {
-            four = signal;
-            segment.b = signal.split()[0];
-            segment.d = signal.split()[2];
-          }
-          if (signal.length === 7) {
-            eight = signal;
-          }
-          // 0, 9, 6:
-          if (signal.length === 6) {
-            nine = signal;
-            six = signal;
-            zero = signal;
-          }
-          // 2, 5:
-          if (signal.length === 5) {
-            two = signal;
-            five = signal;
-          }
-        }
-        return (
-          zero != undefined &&
-          one != undefined &&
-          two != undefined &&
-          three != undefined &&
-          four != undefined &&
-          five != undefined &&
-          six != undefined &&
-          seven != undefined &&
-          eight != undefined &&
-          nine != undefined
-        );
-      };
-      let run = assignValues();
-      while (!run) {
-        // console.log("run");
-        run = assignValues();
-      }
+    let output = "";
 
-      return { zero, one, two, three, four, five, six, seven, eight, nine };
-    };
+    const oneValues = uniqueSignalPatterns
+      .filter((elem) => elem.length === 2)[0]
+      .split("");
+    console.log(oneValues); // right top, right bottom
 
-    const vals = parseValues(uniqueSignalPatterns);
-    console.log(vals);
+    const fourValues = uniqueSignalPatterns
+      .filter((elem) => elem.length === 4)[0]
+      .split("");
+
+    const fourUniqueValues = fourValues.filter(
+      (val) => !oneValues.includes(val)
+    );
+    console.log(fourUniqueValues); // top left, middle
 
     for (const digitalValue of digitOutputValues) {
-      if (
-        digitalValue.length === 2 ||
-        digitalValue.length === 3 ||
-        digitalValue.length === 4 ||
-        digitalValue.length === 7
-      )
-        output++;
+      if (digitalValue.length === 2) output += "1";
+      if (digitalValue.length === 3) output += "7";
+      if (digitalValue.length === 4) output += "4";
+      if (digitalValue.length === 7) output += "8";
+      // * length 5: 2, 3, 5
+
+      const checkerEvery = (arr, target) =>
+        target.every((v) => arr.includes(v));
+      const checkerSome = (arr, target) => target.some((v) => arr.includes(v));
+
+      if (digitalValue.length === 5) {
+        if (checkerEvery(digitalValue.split(""), oneValues)) output += "3";
+        else if (checkerEvery(digitalValue.split(""), fourUniqueValues))
+          output += "5";
+        else output += "2";
+      }
+
+      // * length 6: 0, 6, 9
+      if (digitalValue.length === 6) {
+        if (checkerEvery(digitalValue.split(""), oneValues)) output += "9";
+        // FIXME: checkersome messing me up
+        else if (checkerSome(digitalValue.split(""), oneValues)) output += "6";
+        else output += "0";
+      }
     }
+    console.log(output);
+    outputArr.push(parseInt(output));
   }
-  return output;
+  console.log(outputArr);
+  for (const a of outputArr) console.log(a);
+  let initialValue = 0;
+  return outputArr.reduce(
+    (prevVal, currentVal) => prevVal + currentVal,
+    initialValue
+  );
 };
 
 console.log(findDigits(data));
